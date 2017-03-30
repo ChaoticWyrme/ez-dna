634ad4cada58f589dfdf3e450dcfea31afe1fb5b
Mason Osborn <mason.osborn@gmail.com>
2017-03-30 12:36:37 -0400

    Update app.js
    a bunch plus changed single quotes to double quotes.

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,20 +1,27 @@
 "use strict";
+const aminoMap = {
+  "a":{"a":{}},
+  "u":{},
+  "c":{},
+  "g":{},
+}
+
 function process(event) {
-  var mDNA = document.getElementById('mainstr'); // main strand
-  var cDNA = document.getElementById('complstr'); // complimentary strand
-  var mRNA = document.getElementById('mrna'); // mRNA field; codons
-  var tRNA = document.getElementById('trna'); // anti-codons
-  var amino = document.getElementById('amino'); // amino acids not yet implemented.
-  let filled = (mDNA.value != '')+(cDNA.value != '')+(mRNA.value != '')+(tRNA.value != ''); // # of fields with more than an empty string
+  var mDNA = document.getElementById("mainstr").value; // main strand
+  var cDNA = document.getElementById("complstr").value; // complimentary strand
+  var mRNA = document.getElementById("mrna").value; // mRNA field; codons
+  var tRNA = document.getElementById("trna").value; // anti-codons
+  var amino = document.getElementById("amino"); // amino acids not yet implemented.
+  let filled = (mDNA != "")+(cDNA != "")+(mRNA != "")+(tRNA != ""); // # of fields with more than an empty string
   if(filled = 1) { // one field filled
-    if(mDNA.value != '') {
-      let dna = mDNA.value;
+    if(mDNA != "") {
+      let dna = mDNA;
       let bp = basePair(dna);
       let rna = DNAtRNA(bp);
-      cDNA.value = bp;
-      mRNA.value = rna;
-      tRNA.value = DNAtRNA(dna);
-      amino.value = RNAtAmino(rna);
+      cDNA = bp;
+      mRNA = rna;
+      tRNA = DNAtRNA(dna);
+      amino.innerText = RNAtAmino(rna);
     }
   } else if(filled < 1) { // zero fields filled
     
@@ -25,19 +32,26 @@ function process(event) {
 }
 
 function DNAtRNA(dna) { // converts DNA to RNA
-  return dna.replace(/t/g,"u").replace(/t/g,"U");
+  return dna.replace(/t/gi,"U");
 }
 
 function basePair(strand) { // base pairs strand
   // need to check if rna or dna with str.find()
-  return "";
+  if(/u/i.test(strand) && !/t/i.test(strand)) {
+    return strand.toLowerCase().replace(/u/g,"A").replace(/a/g,"U").replace(/c/g,"G").replace(/g/g,"C");
+  } else if(/t/i.test(strand) && !/u/i.test(strand)) {
+    return strand.toLowerCase().replace(/t/g,"A").replace(/a/g,"T").replace(/c/g,"G").replace(/g/g,"C");
+  } else {
+    throw new StrandError("both DNA and RNA");
+  }
+}
 
 function RNAtAmino(rna) { // turns RNA into amino acid chain.
   return "";
 }
 
 function findAmino(codon) {
-  if(typeof codon == 'string' && codon.length == 3) {
+  if(typeof codon == "string" && codon.length == 3) {
     
   }
 }
@@ -46,9 +60,16 @@ function AminoToProtein() { // turns amino acid chain into protein. Probably not
 
 }
 
+function StrandError(message) {
+  this.name = "StrandError";
+  if(message) {
+    this.message = message;
+  } else this.message = "Invalid DNA/RNA strand";
+}
+
 function init() {
-  var submit = document.getElementById('dnaSubmit');
-  submit.addEventListener('click', process, false);
+  var submit = document.getElementById("dnaSubmit");
+  submit.addEventListener("click", process, false);
 }
 
 window.addEventListener('load', init, false);
